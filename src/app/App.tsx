f5f7642613247e0ec1b671bcec5944b6f0d0fadd
jan926 <kjhsds817@gmail.com>
2026-05-14 16:52:32 +0500

    FIx Bugs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,30 @@
+import { useEffect } from "react";
 import { RouterProvider } from "react-router";
 import { ThemeProvider } from "next-themes";
 import { router } from "./routes";
+import { loadSiteSettings } from "../lib/gameStore";
 
 export default function App() {
+  useEffect(() => {
+    const applyTheme = () => {
+      const settings = loadSiteSettings();
+      if (settings.theme === "dark") {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    };
+
+    applyTheme();
+
+    const handleStorageChange = () => {
+      applyTheme();
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
+
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
       <RouterProvider router={router} />
